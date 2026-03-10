bb6be77bf1478236051f1e18d3018ed574080893
shehinshashehinsha174-arch <shehinshashehinsha174@gmail.com>
2026-03-10 14:15:41 +0530

    creat login page and signup page.produsts showed in home page

--- ecomerceproject/urls.py
+++ ecomerceproject/urls.py
@@ -25,6 +25,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
     path('all_products/',views.all_products, name='allproducts'),
+    path('login/',views.sign_in, name='login'),
+    path('signup/',views.sign_up, name='signup'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -3,9 +3,16 @@ from shop.models import Product
 
 # Create your views here.
 def home(request):
-    return render(request,"index.html")
+    product = Product.objects.all()
+    return render(request,"index.html",{"products": product})
 
-def all_products(request):
+def all_products(request):#this is allproduct view function which is used to show all the products in the database
     product = Product.objects.all()
     return render(request,"all products.html",{"products": product})
 
+
+def sign_in(request):
+    return render(request,"login.html")
+
+def sign_up(request):
+    return render(request,"sigup.html")
