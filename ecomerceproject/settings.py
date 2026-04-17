56b06d8271227c061d13be69e8a72df97119d37a
shehinshashehinsha174-arch <shehinshashehinsha174@gmail.com>
2026-04-17 09:52:47 +0530

    gitignore file added

--- ecomerceproject/settings.py
+++ ecomerceproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-o36!za+abo2eeo@q^fafih96+5^g0(4px57dx%l(-op7gil=c=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://ecomerce-application.onrender.com/', 'localhost', '127.0.0.1']
 
 
 # Application definition
